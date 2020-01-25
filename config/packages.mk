ed9ea8525063247c82fa31181581bd7bd7dd7481
Genkzsz11 <Genkzsz11@gmail.com>
2020-01-26 03:51:52 +0700

    overlay: remove cutout overlay
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -9,10 +9,6 @@ PRODUCT_PACKAGES += \
     WallpaperPicker2 \
     Lawnchair
 
-# Overlays
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Allows registering device to Google easier for gapps
 # Integrates package for easier Google Pay fixing
 PRODUCT_PACKAGES += \
